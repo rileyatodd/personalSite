d3a520b7d19807793d4b376436a4e12cbd4ae1fe
Riley Todd <rileyatodd@gmail.com>
2015-05-09 15:24:20 -0400

    bugfixing projects 404

--- routes/projects.js
+++ routes/projects.js
@@ -6,19 +6,19 @@ var express = require('express'),
 /* GET home page. */
 router.get('/:projectQuery', function(req, res, next) {
   // res.render('project', {projectName: req.params.projectQuery});
-  var projects = db.get('projects');
-  var project;
-  projects.find({path: req.params.projectQuery})
-    .success(function(docs) {
-      project = docs[0];
-      res.render('project', project);
-    })
-    .error(function(err) {
-      res.render('project', {});
-    })
-    .complete(function(){
-      res.render('project', {});
-    });
+  // var projects = db.get('projects');
+  // var project;
+  // projects.find({path: req.params.projectQuery})
+  //   .success(function(docs) {
+  //     project = docs[0];
+  //     res.render('project', project);
+  //   })
+  //   .error(function(err) {
+  //     res.render('project', {});
+  //   })
+  //   .complete(function(){
+  //     res.render('project', {});
+  //   });
   res.render('project', {name: "test"});
 });
 

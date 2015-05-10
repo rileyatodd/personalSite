b0c9a17abbfc251b6cbeeefd8a05810cf6e62643
Riley Todd <rileyatodd@gmail.com>
2015-05-10 13:24:34 -0400

    Fixed (i think) the problems with the projects routing

--- routes/projects.js
+++ routes/projects.js
@@ -1,25 +1,21 @@
 var express = require('express'),
   router = express.Router(),
   mongo = require('mongodb'),
-  // db = require('monk')('localhost/personalSiteDb');
+  db = require('monk')('localhost/personalSiteDb');
 
 /* GET home page. */
 router.get('/:projectQuery', function(req, res, next) {
-  // res.render('project', {projectName: req.params.projectQuery});
-  // var projects = db.get('projects');
-  // var project;
-  // projects.find({path: req.params.projectQuery})
-  //   .success(function(docs) {
-  //     project = docs[0];
-  //     res.render('project', project);
-  //   })
-  //   .error(function(err) {
-  //     res.render('project', {});
-  //   })
-  //   .complete(function(){
-  //     res.render('project', {});
-  //   });
-  res.render('project', {name: "test"});
+  var projects = db.get('projects');
+  var project;
+  projects.find({path: req.params.projectQuery})
+    .success(function(docs) {
+      project = docs[0];
+      res.render('project', project);
+    })
+    .error(function(err) {
+      console.log(err);
+    })
+
 });
 
 module.exports = router;

b288158e0cc6db91bf00f74208a6a88d475e19e2
Riley Todd <rileyatodd@gmail.com>
2017-03-02 18:48:41 -0600

    Initial work on stripping out bloat from third party libraries

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,21 @@
-//Change colors of headers when they are selected from the navbar to show the
-//button works even if it doesn't scroll the page 
-$(document).ready(function(){
-	$('ul.navbar-nav').on('click', 'li', function(){
-		if (!$(this).hasClass('dropdown')) {
-			var href = $(this).find('a').prop('href');
-			var anchor = href.slice(href.indexOf('#'));
-			$('.highlighted').removeClass('highlighted');
-			$(anchor).addClass('highlighted');
+function onClickOutside(element, f) {
+	window.addEventListener('click', function(e) {
+		if (!element.contains(e.target)) {
+			f()
 		}
-	});
-});
+	})
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	document
+		.querySelectorAll('.dropdown')
+		.forEach(function(container) {
+			container.classList.add('engaged')
+			container.addEventListener('click', function(el) {
+				this.classList.toggle('open')
+			})
+			onClickOutside(container, function() {
+				container.classList.remove('open')
+			})
+		})
+})

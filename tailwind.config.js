57037d0afc62bf00eee0e79696c5a2762905f9c5
Riley Todd <rileyatodd@gmail.com>
2020-03-21 14:53:19 -0500

    WIP

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    colors: {
+      blue: {
+        0: '#047CB0',
+        1: '#61c7f5'
+      },
+      white: '#ffffff',
+      black: '#000000'
+    },
+    extend: {
+      screens: {
+        'maxlg': {'max': '760px'}
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
--- watch.js
+++ watch.js
@@ -1,14 +1,28 @@
-var filewatcher = require('filewatcher')()
+var FileWatcher = require('filewatcher')
 var exec = require('child_process').exec
 
-filewatcher.add('views')
-filewatcher.add('articles')
+var jadeWatcher = FileWatcher()
+jadeWatcher.add('views')
+jadeWatcher.add('articles')
 
-filewatcher.on('change', () => {
+jadeWatcher.on('change', () => {
   exec("npm run build", (err, stdout, stderr) => {
     err && filewatcher.removeAll()
     console.log(stdout)
     console.error(stderr)
   })
 })
+
+var tailwindWatcher = FileWatcher()
+tailwindWatcher.add('src/index.css')
+tailwindWatcher.add('tailwind.config.js')
+
+tailwindWatcher.on('change', () => {
+  exec("npm run build:tw", (err, stdout, stderr) => {
+    err && tailwindWatcher.removeAll()
+    console.log(stdout)
+    console.log(stderr)
+  })
+})
+
 

7114a73483b900edcdb654357d5b0c0e7f420717
Riley <fake@mail.com>
2021-11-19 14:13:10 -0500

    Extract react rendering code into buildHtml.js

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { renderToStaticMarkup } from 'react-dom/server'
-
-let articles = 
--- src/lib/buildHtml.js
+++ src/lib/buildHtml.js
@@ -1,17 +1,23 @@
 const fs = require('fs').promises
 const path = require('path')
 const R = require('ramda')
+const React = require('react')
+const { renderToStaticMarkup } = require('react-dom/server')
+
+// This allows subsequent requires to correctly process jsx
+// as such this buildHtml file MUST be required BEFORE any component files
+require('@babel/register')({presets: [require.resolve('@babel/preset-react'),
+                                      require.resolve('@babel/preset-env')]})
 
 const { pipeP, flatMapP } = require('./util.js')
 
 function renderToDir({ outputDir, getData, render }) {
-  pipeP(cleanDir(outputDir),
-        getData,
-        flatMapP(data => pipeP(render(data), output => R.mergeRight(data, {output}))),
-        flatMapP(data => fs.writeFile(
-                           path.join(outputDir, data.fileName.replace('.md', '.html')), 
-                           data.output)),
-        _ => process.exit(0))
+  return pipeP(cleanDir(outputDir),
+               getData,
+               flatMapP(data => pipeP(render(data), output => R.mergeRight(data, {output}))),
+               flatMapP(data => fs.writeFile(
+                                  path.join(outputDir, data.fileName.replace('.md', '.html')), 
+                                  data.output)))
 }
 
 function cleanDir(outputDir) {
@@ -21,4 +27,12 @@ function cleanDir(outputDir) {
            .then(_ => fs.mkdir(outputDir, { recursive: true }))
 }
 
-module.exports = { renderToDir, cleanDir }
+function ensureDir(dir) {
+  return fs.access(dir).catch(_ => fs.mkdir(dir, { recursive: true }))
+}
+
+function renderReact(component, props) {
+  return renderToStaticMarkup(React.createElement(component, props))
+}
+
+module.exports = { renderToDir, cleanDir, ensureDir, renderReact }

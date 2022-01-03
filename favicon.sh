7f53ecc03aa717b7d62c5acc8e85dc34c76c1380
rt <rileyatodd@gmail.com>
2022-01-03 16:28:06 -0500

    update favicon.sh to move icon to assets/ and clean up

--- favicon.sh
+++ favicon.sh
@@ -1,3 +1,9 @@
 for size in 16 32 48; do
   magick convert favicon.png -background white -resize ${size}x${size} -flatten favicon_${size}.bmp
 done
+
+magick favicon*.bmp favicon.ico
+
+mv favicon.ico assets/
+
+rm favicon*.bmp
